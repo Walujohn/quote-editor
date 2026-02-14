8ffcf19b7616638e99e29da6ca99c9d744c79bba
root <root@LAPTOP-I3H5RCJ7.localdomain>
2026-02-14 14:04:09 -0500

    Add empty state to index page

---
+++ test/system/quotes_empty_state_test.rb
@@ -0,0 +1,26 @@
+require "application_system_test_case"
+
+class QuotesEmptyStateTest < ApplicationSystemTestCase
+  setup do
+    @user = users(:accountant) # or however your fixture user is named
+    login_as @user
+  end
+
+  test "shows empty state when there are no quotes" do
+    Quote.where(company: @user.company).delete_all
+
+    visit quotes_path
+
+    assert_text "You don't have any quotes yet!"
+  end
+
+  test "hides empty state when there are quotes" do
+    Quote.where(company: @user.company).delete_all
+    Quote.create!(name: "Capybara quote", company: @user.company)
+
+    visit quotes_path
+
+    assert_no_text "You don't have any quotes yet!"
+    assert_text "Capybara quote"
+  end
+end
