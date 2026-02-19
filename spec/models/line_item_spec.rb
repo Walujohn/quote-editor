6505af1643714684cd45e861d21b48ef0d6971c0
root <root@LAPTOP-I3H5RCJ7.localdomain>
2026-02-19 09:40:08 -0500

    Add total sticky bar

--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe LineItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "returns the total price of the line item" do
+    line_item = LineItem.new(quantity: 5, unit_price: 10)
+    expect(line_item.total_price).to eq(50)
+  end
 end
--- spec/models/quote_spec.rb
+++ spec/models/quote_spec.rb
@@ -25,4 +25,14 @@ RSpec.describe Quote, type: :model do
     quote = build(:quote, company: nil)
     expect(quote).not_to be_valid
   end
+
+  it "calculates the total price of the quote" do
+    quote = create(:quote)
+    date  = create(:line_item_date, quote: quote)
+
+    create(:line_item, quantity: 2, unit_price: 10, line_item_date: date)
+    create(:line_item, quantity: 3, unit_price: 20, line_item_date: date)
+
+    expect(quote.total_price).to eq(80)
+  end
 end
